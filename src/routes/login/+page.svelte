33e3c807c4198a85c981fae8056343b7e0658f81
Patrik835 <22IMC10257@fh-krems.ac.at>
2023-11-16 13:57:23 +0100

    fixed login deletion

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,17 @@
 <script>
-  import Example from "$lib/components/Example.svelte";
-  export let data;
-  console.log(data);
-</script>
+    import Login from "$lib/components/login/login.svelte";
+    import Circle from "$lib/components/login/circle.svelte";
+    import Navbar from "$lib/components/navbar.svelte";
+  </script>
+  
+  <body class="background w-full h-full justify-center overflow-x-hidden">
+    <Navbar />
+    <div class="h-screen flex flex-col items-center justify-center">
+      <div class="">
+        <Circle />
+      </div>
+      <div class="absolute z-10">
+        <Login />
+      </div>
+    </div>
+  </body>

453fdbad32aceab485da4e447dddb2f4670d112c
t0nkisa <antanitonev03@gmail.com>
2023-11-20 14:08:40 +0100

    added profile info component for large screens with dummy data

---
+++ src/lib/components/profilePage/ProfileInformation.svelte
@@ -0,0 +1,16 @@
+<ul
+  class="flex flex-col gap-5 rounded-lg p-4 text-white bg-[url('/images/savana.jpg')] bg-cover w-[250px] h-[250px] relative before:content-[''] before:absolute before:w-full before:h-full before:backdrop-blur-sm before:top-0 before:left-0 before:rounded-lg shadow-sm shadow-black"
+>
+  <li class="border-b border-b-white flex justify-between z-10">
+    <p>Name</p>
+    <p>Placeholder</p>
+  </li>
+  <li class="border-b border-b-white flex justify-between z-10">
+    <p>Email</p>
+    <p>Placeholder</p>
+  </li>
+  <li class="border-b border-b-white flex justify-between z-10">
+    <p>Password</p>
+    <p>Placeholder</p>
+  </li>
+</ul>

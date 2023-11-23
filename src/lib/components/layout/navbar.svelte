bb0ee4e640e948bcf00254f6fe3f5932bcff64f1
Patrik835 <22IMC10257@fh-krems.ac.at>
2023-11-23 14:06:39 +0100

    Revert "navbar active changes"
    This reverts commit 1cd73522103ad6890199e693941a875a310dcb66.
    
    revert

--- src/lib/components/layout/navbar.svelte
+++ src/lib/components/layout/navbar.svelte
@@ -8,36 +8,48 @@ import {page} from '$app/stores';
     <div
       class=" container mx-auto flex md:justify-between justify-center items-center"
     >
-    <div  class="logo-container md:block hidden ">
+    <div  class="logo-container md:block hidden ml-8">
       <a href="/" class="flex items-center">
         <img src="/images/logo.jpg" alt="Logo" class="rounded-full object-cover w-20 h-20" />
         <span class="ml-2 font-bold text-2xl">Safari Hangout</span>
       </a>
     </div>
       <div class="flex items-center space-x-4">
-        <div class={$page.url.pathname == '/events'}>
+        <div class={$page.url.pathname == '/events' ? "pt-11" : ""}>
             <a href="/events"
               class=" font-semibold order-1 px-8 py-4 shadow-lg bg-white text-black rounded hover:bg-gray-300"
             >
               Events
             </a>
+            {#if $page.url.pathname == '/events' }
+                <hr class="w-full h-1.5 mx-auto my-5  bg-gradient-to-r from-[#ffe700] to-[#ff0000] rounded ">
+                  
+            {/if}
         </div>
         <div class="md:order-3 order-2">
-          <div class={$page.url.pathname == '/profile'}>
+          <div class={$page.url.pathname == '/profile' ? "pt-9" : ""}>
             <a href="/profile " 
               class="active font-semibold  md:shadow-none shadow-lg px-5 py-4 bg-white md:bg-transparent text-black rounded hov"
               >
+              
               <i class="fa-solid fa-user p-2 text-2xl rounded-full bg-black text-white"></i>
+             
             </a>
+            {#if $page.url.pathname == '/profile' }
+            <hr class="w-full h-1.5 mx-auto my-5  bg-gradient-to-r from-[#ffe700] to-[#ff0000] rounded ">
+            {/if}
           </div>
         </div>
         <div class="md:order-2 order-3">
-         <div class={$page.url.pathname == '/safari'}>
+         <div class={$page.url.pathname == '/safari' ? "pt-11" : ""}>
             <a href="/safari"
               class="active font-semibold  shadow-lg px-8 py-4 bg-white text-black rounded hover:bg-gray-300"
             >
               Safari
             </a>
+            {#if $page.url.pathname == '/safari' }
+              <hr class="w-full h-1.5 mx-auto my-5  bg-gradient-to-r from-[#ffe700] to-[#ff0000] rounded ">  
+            {/if}
           </div> 
          </div> 
       </div>

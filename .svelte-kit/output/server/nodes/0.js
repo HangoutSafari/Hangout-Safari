735bd02ecea482ab3d89bd95dc93da8d8171ccab
Simon P <poto0002@hz.nl>
2023-11-24 10:51:54 +0100

    configured tsconfing

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.8aad479f.js","_app/immutable/chunks/index.2921e8e9.js","_app/immutable/chunks/stores.209088dc.js","_app/immutable/chunks/singletons.b7a9429b.js"];
-export const stylesheets = ["_app/immutable/assets/0.a047bdc8.css","_app/immutable/assets/navbar.b5206811.css"];
-export const fonts = [];

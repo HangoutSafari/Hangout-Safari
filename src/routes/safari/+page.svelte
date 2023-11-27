405ae7daf2c2608711406100b0dbfeacd22b9842
Simon P <poto0002@hz.nl>
2023-11-27 16:49:43 +0100

    finally fixed the f*cking side bar

--- src/routes/safari/+page.svelte
+++ src/routes/safari/+page.svelte
@@ -4,12 +4,12 @@
   </script>
 
 <div
-  class="h-full w-[100%] flex flex-col lg:flex-row items-center background overflow-x-none overflow-y-none"
+  class="h-[90%] w-[100%] flex flex-col lg:flex-row items-center background overflow-x-none overflow-y-none"
 >
-<div class="sm:h-full self-start">
-  <SideNavbar />
-</div>
-  <div class="h-full flex flex-col w-full">
+  <div class="sm:h-full w-full lg:w-[25%] self-start">
+    <SideNavbar />
+  </div>
+  <div class="h-full flex flex-col w-full p-10">
     <RenderingContext />
   </div>
 </div>

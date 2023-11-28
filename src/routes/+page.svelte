2cb826790f15961795ec4e2724dd99d83dd5bf24
Simon P <poto0002@hz.nl>
2023-11-28 12:01:59 +0100

    created landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,11 @@
   console.log(data);
 </script>
 
-<div />
+<main class="h-full w-full flex flex-col items-center justify-center">
+    <h1 class="text-3xl justify-start ">Welcome, to Safari Hangout</h1>
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script> 
+    <dotlottie-player src="https://lottie.host/af1710d7-649a-4b4f-83ab-7764ee7f796d/BHFb90wVs9.json" background="transparent" speed="2" style="width: 70%; height: 70%;" autoplay></dotlottie-player>
+</main>
 
 <style>
 </style>

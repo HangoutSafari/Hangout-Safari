8ab1d5b492dedc69d176958eadef76a85a0b2882
Simon-Manasse <simo0047@hz.nl>
2023-11-29 14:42:26 +0100

    Build test

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [sveltekit()],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
-    exclude: isGitHubActions ? ["src/tests/unit/api/animals.api.response.test.ts"] : [],
+    exclude: isGitHubActions ? ["src/tests/unit/api/animals.api.response.test.ts","src/tests/unit/profile/profile.nonExistentUri.test.ts","src/tests/unit/profile/profile.response.test.ts","src/tests/unit/api/animals.api.dataResponse.test.ts"] : [],
   },
 });

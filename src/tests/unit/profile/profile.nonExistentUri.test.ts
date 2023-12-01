9cdbea0f8a5d9aaab7fddfbe08c5d8b5980f7800
Simon P <poto0002@hz.nl>
2023-12-01 14:21:42 +0100

    first push after new gitignore

--- src/tests/unit/profile/profile.nonExistentUri.test.ts
+++ src/tests/unit/profile/profile.nonExistentUri.test.ts
@@ -2,7 +2,7 @@ import { describe,it,expect } from "vitest";
 
 describe("Profile can be called", () => {
     it("check if the response is 404", async () => {
-      const response = await fetch('http://localhost/profilee');
+      const response = await fetch('http://localhost:5173/profilee');
       expect(response.status).toBe(404);
     });
   });
--- src/tests/unit/profile/profile.response.test.ts
+++ src/tests/unit/profile/profile.response.test.ts
@@ -2,8 +2,7 @@ import { describe, it, expect } from "vitest";
 
 describe("Profile can be called", () => {
   it("opens the profile page", async () => {
-    const response = await fetch('http://localhost/profile');
+    const response = await fetch('http://localhost:5173/profile');
     expect(response.status).toBe(200);
   });
 });
-

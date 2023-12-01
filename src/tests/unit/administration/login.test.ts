a5307b43044988f426abcfdb307b34d6c2a68995
t0nkisa <antanitonev03@gmail.com>
2023-12-01 14:21:39 +0100

    added ports to test

--- src/tests/unit/administration/login.test.ts
+++ src/tests/unit/administration/login.test.ts
@@ -2,6 +2,6 @@ import { describe, it, expect } from "vitest";
 
 describe("test if login path works", () => {
   it("should return request code 200", async () => {
-    const response = await fetch("http://localhost/login");
+    const response = await fetch("http://localhost:5173/login");
     expect(response.status).toBe(200);    
   })});
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,18 @@ const isGitHubActions = process.env.CI === "true";
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
-    exclude: isGitHubActions ? ["src/tests/unit/api/animals.api.response.test.ts","src/tests/unit/profile/profile.nonExistentUri.test.ts","src/tests/unit/event_slug/event_slug.response.test.ts","src/tests/unit/profile/profile.response.test.ts","src/tests/unit/api/animals.api.dataResponse.test.ts"] : ["/node_modules"],
+    exclude: isGitHubActions
+      ? [
+          "src/tests/unit/api/animals.api.response.test.ts",
+          "src/tests/unit/profile/profile.nonExistentUri.test.ts",
+          "src/tests/unit/event_slug/event_slug.response.test.ts",
+          "src/tests/unit/profile/profile.response.test.ts",
+          "src/tests/unit/api/animals.api.dataResponse.test.ts",
+          "src/tests/unit/administration/login.test.ts",
+          "src/tests/unit/administration/register.test.ts",
+          "src/tests/unit/profile/profile.nonExistentUri.test.ts",
+          "src/tests/unit/profile/profile.response.test.ts"
+        ]
+      : ["node_modules"],
   },
 });

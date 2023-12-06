e25cc46c0e5306818fcf5afd8a275e5b6ed6e4de
t0nkisa <antanitonev03@gmail.com>
2023-12-06 13:22:59 +0100

    Separate FormInput into different components

---
+++ src/lib/components/form/FormTitle.svelte
@@ -0,0 +1,23 @@
+<script>
+  let underlined = true;
+</script>
+
+<div class="{$$props.class} w-[400px]">
+  <i
+    class="fa-solid fa-a absolute text-center mt-3 px-1 border-orange-300 border-r-2 text-gray-400"
+  />
+  <input
+    on:blur={() => (underlined = true)}
+    on:click={() => (underlined = false)}
+    type="text"
+    id="title"
+    placeholder="Title"
+    class="w-full px-7 py-2 focus:outline-none focus:ring focus:ring-orange-400 bg-transparent"
+  />
+  {#if underlined}
+    <hr class="border-2 rounded border-orange-400" />
+  {/if}
+  {#if !underlined}
+    <hr class="border-2 rounded border-transparent" />
+  {/if}
+</div>

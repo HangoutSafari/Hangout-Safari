0a42faba50f834d29dcabddb383d171e84fe1afe
t0nkisa <antanitonev03@gmail.com>
2023-12-06 13:22:59 +0100

    Separate FormInput into different components

---
+++ src/lib/components/form/FormDescription.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="{$$props.class} w-[400px]">
+  <i
+    class="fa-solid fa-pen absolute text-center mt-3 px-1 border-orange-300 border-r-2 text-gray-400"
+  />
+  <textarea
+    id="description"
+    rows="4"
+    class="w-full px-7 py-2 outline-none ring ring-orange-400"
+  >
+    Description...
+  </textarea>
+</div>
--- src/routes/events/crud/+page.svelte
+++ src/routes/events/crud/+page.svelte
@@ -1,28 +1,18 @@
 <script>
-  import FormInput from "$lib/components/form/FormInput.svelte";
+  import FormTitle from "$lib/components/form/FormTitle.svelte";
+  import FormDate from "$lib/components/form/FormDate.svelte";
+  import FormTime from "$lib/components/form/FormTime.svelte";
+  import FormDescription from "$lib/components/form/FormDescription.svelte";
+  import FormImage from "$lib/components/form/FormImage.svelte";
+  import FormAddress from "$lib/components/form/FormAddress.svelte";
 </script>
 
 <div class="w-full h-full flex justify-center items-center flex-col gap-6">
-  <FormInput
-    id="title"
-    type="text"
-    icon="fa-solid fa-t"
-    placeholder="Title"
-    class=""
-  />
-  <FormInput id="date" type="date" icon="fa-regular fa-calendar" />
-  <FormInput id="time-start" type="time" icon="fa-regular fa-clock" />
-  <FormInput id="time-end" type="time" icon="fa-regular fa-clock" />
-  <FormInput
-    id="image"
-    type="file"
-    icon="fa-solid fa-image"
-    accept=".png, .jpg, .jpeg"
-  />
-  <FormInput
-    id="description"
-    type="textarea"
-    icon="fa-solid fa-pen"
-    placeholder="Description..."
-  />
+  <FormTitle />
+  <FormDate />
+  <FormTime id="time-start" />
+  <FormTime id="time-end" />
+  <FormImage />
+  <FormDescription />
+  <FormAddress />
 </div>

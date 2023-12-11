95d8d9509b02a4d54a509ce1067ccac12320af81
t0nkisa <antanitonev03@gmail.com>
2023-12-11 07:05:56 +0100

    create page made

--- src/lib/components/form/FormAddress.svelte
+++ src/lib/components/form/FormAddress.svelte
@@ -2,7 +2,7 @@
   let underlined = true;
 </script>
 
-<div class="{$$props.class} w-[400px]">
+<div class="{$$props.class} w-full">
   <i
     class="fa-solid fa-location-dot absolute text-center mt-3 px-1 border-orange-300 border-r-2 text-gray-400"
   />
---
+++ src/routes/events/create/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Map from "$lib/components/eventPage/Map.svelte";
+  import FormAddress from "$lib/components/form/FormAddress.svelte";
+  import FormDate from "$lib/components/form/FormDate.svelte";
+  import FormDescription from "$lib/components/form/FormDescription.svelte";
+  import FormImage from "$lib/components/form/FormImage.svelte";
+  import FormTime from "$lib/components/form/FormTime.svelte";
+  import FormTitle from "$lib/components/form/FormTitle.svelte";
+</script>
+
+<div class="w-[80%] mx-auto my-2 py-8">
+  <h1 class="text-2xl font-bold mb-10">Create an event</h1>
+
+  <div
+    class="w-[90%] md:grid md:grid-cols-3 md:grid-rows-5 md:grid-flow-row md:gap flex flex-col gap-4 -8 mx-auto"
+  >
+    <FormTitle class="2xl:w-[350px]" />
+    <FormTime id="time-start" class="2xl:w-[350px]" />
+    <FormTime id="time-end" class="2xl:w-[350px]" />
+
+    <FormDescription class="row-span-2 2xl:w-[350px]" />
+
+    <FormAddress class="2xl:w-[350px]" />
+    <FormDate class="2xl:w-[350px]" />
+
+    <Map class="min-h-[200px] 2xl:min-h-[0] min-w-full row-span-3 col-span-2" />
+    <FormImage class="2xl:w-[350px] row-span-2 " />
+  </div>
+</div>

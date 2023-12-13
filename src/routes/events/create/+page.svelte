4c0d47a53d72869e720458aed41e58819ed28b54
t0nkisa <antanitonev03@gmail.com>
2023-12-13 10:22:02 +0100

    cities grouped by provinces are now added to the address field

--- src/routes/events/create/+page.svelte
+++ src/routes/events/create/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { lat, long } from "$lib/components/form/store.js";
+
   import Map from "$lib/components/eventPage/Map.svelte";
   import FormAddress from "$lib/components/form/FormAddress.svelte";
   import FormDate from "$lib/components/form/FormDate.svelte";
@@ -6,6 +8,22 @@
   import FormImage from "$lib/components/form/FormImage.svelte";
   import FormTime from "$lib/components/form/FormTime.svelte";
   import FormTitle from "$lib/components/form/FormTitle.svelte";
+  import { address } from "./store.js";
+
+  address.subscribe(async (value) => {
+    console.log(
+      `https://geocode.xyz/${value}?geoit=json&auth=180705050001809255650x22389&region=NL`
+    );
+    const promise = await fetch(
+      `https://geocode.xyz/${value}?geoit=json&auth=180705050001809255650x22389&region=NL`
+    );
+
+    const response = promise.json();
+    const geoData = await response;
+    console.log(geoData);
+    lat.set(geoData.latt);
+    long.set(geoData.longt);
+  });
 </script>
 
 <div class="w-[80%] mx-auto my-2 py-8">
@@ -20,7 +38,7 @@
 
     <FormDescription class="row-span-2 2xl:w-[350px]" />
 
-    <FormAddress class="2xl:w-[350px]" />
+    <FormAddress class="2xl:w-[350px] relative" />
     <FormDate class="2xl:w-[350px]" />
 
     <Map class="min-h-[200px] 2xl:min-h-[0] min-w-full row-span-3 col-span-2" />
---
+++ src/routes/events/create/store.js
@@ -0,0 +1,4 @@
+import { writable } from "svelte/store";
+export const address = writable("");
+
+export const provinces = writable({});

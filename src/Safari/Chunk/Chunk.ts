42868c210ff5da777ebaab21dda351d6822743af
Simon P <poto0002@hz.nl>
2023-12-13 15:11:51 +0100

    added model loader for chunks

--- src/Safari/Chunk/Chunk.ts
+++ src/Safari/Chunk/Chunk.ts
@@ -5,8 +5,9 @@ import { degToRad } from 'three/src/math/MathUtils';
 import { FloorGenerator } from '../Geometry/FloorGenerator';
 import { Floor } from './Floor';
 import { Vegetation } from './Vegetation';
+import { loadModel } from '../ModelLoader/ModelLoader';
 
-export class Chunk extends THREE.Group
+export class Chunk extends THREE.Object3D
 {
     private width: number;
     private height: number;
@@ -14,7 +15,16 @@ export class Chunk extends THREE.Group
     public constructor(widht: number,position:SafariVector)
     {
         super();
-        this.add(new Floor(widht, position));
-        this.add(new Vegetation(1, widht));
+        loadModel('/models/chunk_center.gltf')
+        .then(model =>{
+            const translationMatxi = new THREE.Matrix4();
+            translationMatxi.makeTranslation(position);
+            model.scene.traverse(child =>{
+                child.castShadow = true;
+            });
+            model.scene.scale.set(2.4, 2.4,2.4);
+            model.scene.applyMatrix4(translationMatxi);
+            this.add(model.scene);
+        });
     }
 }

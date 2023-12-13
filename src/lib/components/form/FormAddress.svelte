59a6931aed2f40534672a3a5835eaa1ca0b54adb
t0nkisa <antanitonev03@gmail.com>
2023-12-13 10:22:02 +0100

    cities grouped by provinces are now added to the address field

--- src/lib/components/form/FormAddress.svelte
+++ src/lib/components/form/FormAddress.svelte
@@ -1,26 +1,93 @@
 <script>
-  let underlined = true;
+  import { onMount } from "svelte";
+  import { address, provinces } from "../../../routes/events/create/store.js";
+  import { suggestions } from "./store.js";
   export let value = "";
+  export let cityName;
+  var timeoutId = null;
+  let provinceData;
+  async function AutoSuggest() {
+    timeoutId = null;
+    let streetname = value;
+    const promise = await fetch(
+      `https://geocode.xyz/?geoit=json&auth=180705050001809255650x22389&region=NL&streetname=${streetname}&cityname=${select.value}`
+    );
+    const response = await promise.json();
+    $suggestions = [...Object.keys(response.standard.street)];
+  }
+  function getAutoSuggestions() {
+    if (timeoutId) {
+      window.clearTimeout(timeoutId);
+    }
+    timeoutId = window.setTimeout(AutoSuggest, 1000);
+  }
+
+  let select;
+  onMount(async () => {
+    provinceData = await loadJSON();
+    $provinces = { ...Object.keys(provinceData) };
+    console.log(provinceData);
+    console.log($provinces);
+  });
+  async function loadJSON() {
+    const request = await fetch("../../json/provincesAndCitiesNL.json");
+    const response = await request.json();
+    return response;
+  }
 </script>
 
 <div class="{$$props.class} w-full">
-  <i
-    class="fa-solid fa-location-dot absolute text-center mt-3 px-1 border-orange-300 border-r-2 text-gray-400"
-  />
+  <div class="w-[45%] inline-block">
+    <i
+      class="fa-solid fa-location-dot absolute text-center mt-3 px-1 border-orange-300 border-r-2 text-gray-400"
+    />
+    <input
+      on:keypress={getAutoSuggestions}
+      on:focusout={() => {
+        address.set(value + ", " + select.value);
+      }}
+      type="text"
+      id="address"
+      placeholder="Address"
+      bind:value
+      list="suggestions"
+      class="w-full px-7 py-2 focus:outline-none focus:ring focus:ring-orange-400 bg-transparent"
+    />
+    <datalist id="suggestions">
+      {#each $suggestions as suggestion}
+        <option value={suggestion} />
+      {/each}
+    </datalist>
+  </div>
+  <div class="w-[45%] inline-block float-right">
+    <i
+      class="fa-solid fa-city absolute text-center mt-3 px-1 border-orange-300 border-r-2 text-gray-400"
+    />
+    <select
+      on:focusout={() => {
+        address.set(value + ", " + select.value);
+      }}
+      bind:this={select}
+      class="w-full px-7 py-2 focus:outline-none focus:ring focus:ring-orange-400 bg-transparent"
+    >
+      {#if cityName}
+        <option hidden disabled class="text-gray-400">City</option>
+      {:else}
+        <option selected hidden disabled class="text-gray-400">City</option>
+      {/if}
 
-  <input
-    on:blur={() => (underlined = true)}
-    on:click={() => (underlined = false)}
-    type="text"
-    id="address"
-    placeholder="Address"
-    {value}
-    class="w-full px-7 py-2 focus:outline-none focus:ring focus:ring-orange-400 bg-transparent"
-  />
-  {#if underlined}
-    <hr class="border-2 rounded border-orange-400" />
-  {/if}
-  {#if !underlined}
-    <hr class="border-2 rounded border-transparent" />
-  {/if}
+      {#each Object.entries($provinces) as [index, provinceName]}
+        <optgroup label={provinceName}>
+          {#each provinceData[provinceName] as city}
+            {#if cityName == city.city}
+              <option selected value={city.city}>{city.city}</option>
+            {:else}
+              <option value={city.city}>{city.city}</option>
+            {/if}
+          {/each}
+        </optgroup>
+      {/each}
+    </select>
+  </div>
+  <hr class="border-2 rounded border-orange-400" />
 </div>
--- src/routes/events/[slug]/edit/+page.svelte
+++ src/routes/events/[slug]/edit/+page.svelte
@@ -6,6 +6,8 @@
   import FormImage from "$lib/components/form/FormImage.svelte";
   import FormTime from "$lib/components/form/FormTime.svelte";
   import FormTitle from "$lib/components/form/FormTitle.svelte";
+  import { lat, long } from "$lib/components/form/store.js";
+  import { address } from "../../create/store.js";
 
   const event = {
     title: "Food Lovers",
@@ -13,9 +15,22 @@
     time_start: "13:30",
     time_end: "15:30",
     description: "An event where you can try all kinds of food!",
-    address: "Middelburg",
+    address: "Joost de Moorstraat 21, Vlissingen",
+    street: "Joost de Moorstraat 21",
+    city: "Vlissingen",
     image: "/images/FoodAndDrinkDesignColour.svg",
   };
+  address.set(event.address);
+  address.subscribe(async (value) => {
+    const promise = await fetch(
+      `https://geocode.xyz/${value}?geoit=json&auth=180705050001809255650x22389&region=NL`
+    );
+
+    const response = promise.json();
+    const geoData = await response;
+    lat.set(geoData.latt);
+    long.set(geoData.longt);
+  });
 </script>
 
 <div class="w-[80%] mx-auto my-2 py-8">
@@ -33,7 +48,11 @@
       value={event.description}
     />
 
-    <FormAddress class="2xl:w-[350px]" value={event.address} />
+    <FormAddress
+      class="2xl:w-[350px]"
+      value={event.street}
+      cityName={event.city}
+    />
     <FormDate class="2xl:w-[350px]" value={event.date} />
 
     <Map class="min-h-[200px] 2xl:min-h-[0] min-w-full row-span-3 col-span-2" />

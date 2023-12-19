8acea728b4c22747f650e330a22b33bc515bfe72
Patrik835 <22IMC10257@fh-krems.ac.at>
2023-12-19 21:13:55 +0100

    register validation

--- src/api.js
+++ src/api.js
@@ -1,12 +1,19 @@
-export async function postData(url, { username, email, password }) {
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, email, password }),
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
+export async function postData(url, fields) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fields),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+    return response.status;
+  } catch (error) {
+    console.error('Fetch Error: ', error.message);
+  }
 }

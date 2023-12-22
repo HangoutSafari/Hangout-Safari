c8facc6b5163b89d428238cac5796ad92002b806
Šimon Potočňák <simon.potocnak03@gmail.com>
2023-12-22 14:23:04 +0100

    logic of the animals change is working

--- src/lib/components/safariPage/popupDebug.svelte
+++ src/lib/components/safariPage/popupDebug.svelte
@@ -1,11 +1,11 @@
 <script>
     export let name;
     export let rarity;
-    export let eventName;
+    export let achievedFrom;
 </script>
 
-<div class="p-20 bg-slate-300 rounded-lg">
- <p>Name is {name}</p>
- <p>Achieved forom {eventName}</p>
- <p>Rarity is {rarity}</p>
+<div class="p-20 bg-slate-300 rounded-lg w-full">
+ <p>Name <b> {name}</b></p>
+ <p>Event <b> {achievedFrom} </b></p>
+ <p>Rarity <b> {rarity} </b> </p>
 </div>
--- src/routes/safari/+page.svelte
+++ src/routes/safari/+page.svelte
@@ -1,12 +1,20 @@
-ň<script>
+<script>
   import RenderingContext from "$lib/components/safariPage/renderingContext.svelte";
   import SideNavbar from "$lib/components/safariPage/sidebar.svelte";
+  import PopupDebug from "$lib/components/safariPage/popupDebug.svelte";
   import { onMount } from "svelte";
   
+  let rarity;
+  let achievedFrom;
+  let name;
+
   function onAnimalSelected(event)
   {
-    console.log(event.detail);
+    rarity = event.detail.rarity;
+    achievedFrom = event.detail.achievedFrom;
+    name = event.detail.name;
   }
+  
 
 </script>
 
@@ -18,6 +26,9 @@
   </div>
   <div class="h-full flex flex-col w-full p-4 lg:p-10">
     <RenderingContext on:showAnimal={onAnimalSelected}/>
+  </div>
+  <div class="flex">
+    <PopupDebug rarity ={rarity} achievedFrom={achievedFrom} name = {name} ></PopupDebug>
   </div>
 </div>
 

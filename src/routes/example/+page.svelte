570fb5e6006b64cdb29bf5eff6bb54de6124efe7
t0nkisa <antanitonev03@gmail.com>
2024-01-11 15:12:55 +0100

    added session checking in example

--- src/routes/example/+page.svelte
+++ src/routes/example/+page.svelte
@@ -1,10 +1,19 @@
 <script>
+  import { onMount } from "svelte";
   // data is the data from the page.server.js
-  export let data;
-  console.log(data);
+  // export let data;
+  // console.log(data);
+  onMount(getSession);
+  async function getSession() {
+    const request = await fetch("http://localhost:3010/auth/session", {
+      credentials: "include",
+    });
+    const response = await request.json();
+    return { message: `data from the server ${response}` };
+  }
 </script>
 
 <div>
   <h1>Nice example of an very simple component</h1>
-  <p>{data.message}</p>
+  <!-- <p>{data.message}</p> -->
 </div>

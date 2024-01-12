b7d4c064cd406e06f49a54af1f8b70038b667139
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-01-12 15:27:41 +0100

    finished side menu items

--- src/lib/components/safariPage/SideBarItem.svelte
+++ src/lib/components/safariPage/SideBarItem.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <div
-    class="flex flex-row justify-items-center shadow-lg p-2 md:container w-full lg:w-[90%] rounded-2xl bg-white"
+    class="flex flex-row justify-items-center shadow-lg p-2 md:container w-full lg:w-[90%] rounded-2xl bg-white hover:scale-110 duration-150 hover:cursor-grab"
 >
-    <div class="flex flex-col w-[130px] lg:w-[160px] h-[80px] lg:h-[100px]">
+    <div class="flex flex-col w-[130px] lg:w-[160px] h-[80px] lg:h-[100px] shadow-lg">
         <div />
         <img
             src="/images/modelImages/elephant.png"
@@ -16,8 +16,8 @@
             class="h-12 bg-orange-400 rounded-lg p-1 h-full "
         />
     </div>
-    <div class="hidden lg:flex flex-col ml-10 text-2xl text-gray-400">
-        <h2>Name</h2>
+    <div class="hidden lg:flex flex-col ml-10 ">
+        <h2 class="text-2xl text-gray-400">Name</h2>
         <div
             class="flex text-sm gap-2 content-around pt-1 text-black text-opacity-40"
         >
@@ -28,5 +28,14 @@
                 </div>
             </div>
         </div>
+        <div class="flex text-sm gap-2 content-around pt-1 text-black text-opacity-40">
+            <div class="flex justify-items-end">
+                <div class="flex">
+                    <i class=" mr-2 fa-solid fa-location-dot" />
+                    <p>{dateObtained}</p>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>

1f5f80d78afe14903cd81f7f164d63b0640d863b
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-01-12 13:46:30 +0100

    created basic aside menu items

---
+++ src/lib/components/safariPage/SideBarItem.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    
+</script>
+
+<div class="flex flex-col w-[90%] bg-white rounded-lg shadow-xl">
+    <div class="flex flex-row items-center p-6">
+        <div class="w-1/3 h-20 bg-slate-100 flex flex-col items-center">
+            <img alt="animalImage" class="w-[100%] h-[100%]" src="images/modelImages/deer.png">
+        </div>
+        <div class="w-2/3 text-2xl text-black ml-10">
+            Deer
+        </div>
+    </div>
+</div>
---
+++ src/lib/components/safariPage/Sidebar.svelte
@@ -0,0 +1,72 @@
+<script>
+  import Header from "../events/header.svelte";
+  import SideBarItem from "./SideBarItem.svelte"
+
+  export let header;
+</script>
+
+<main class="sm:h-full w-full">
+  <!-- Sidebar -->
+  <aside
+    class="font-sans w-full sm:h-full mr-4 bg-[#D9D9D9] text-gray-200 flex flex-col drop-shadow-lg"
+  >
+    <div class="sticky top-20">
+      <!-- Header name -->
+      <Header text="{header}" class="hidden sm:inline-block"/>
+
+      <!-- Search bar-->
+      <label
+        for="search"
+        class="mx-5 my-3 sm:m-5 h-8 bg-[#BBBBBB] flex items-center rounded-xl focus-within:border-4 border-sky-400/30"
+      >
+        <i class="fas fa-search mx-2" />
+        <input
+          type="text"
+          id="search"
+          placeholder="Search..."
+          class="outline-none border-none bg-transparent placeholder-gray-200"
+        />
+        
+      </label>
+    </div>
+    <div class="flex flex-col h-[full] gap-3 overflow-y-auto overflow-x-clip items-center ">
+  
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+      <SideBarItem/>
+    </div>
+  </aside>
+</main>

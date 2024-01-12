ccd1655f8f5b8973c7d6dd4357676e3c1ef03cf9
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-01-12 15:13:05 +0100

    created the animal side bar item

--- src/lib/components/safariPage/SideBarItem.svelte
+++ src/lib/components/safariPage/SideBarItem.svelte
@@ -6,16 +6,20 @@
 </script>
 
 <div
-    class="flex flex-row justify-items-center border-b border-white p-3 md:container md:mx-auto bg-white"
+    class="flex flex-row justify-items-center shadow-lg p-2 md:container w-full lg:w-[90%] rounded-2xl bg-white"
 >
-    <div class="flex flex-col">
+    <div class="flex flex-col w-[130px] lg:w-[160px] h-[80px] lg:h-[100px]">
+        <div />
         <img
-            src="/images/modelImages/crocodile.png"
+            src="/images/modelImages/elephant.png"
             alt="Profile Image"
-            class="h-12 bg-orange-400 rounded-lg p-1"
+            class="h-12 bg-orange-400 rounded-lg p-1 h-full "
         />
+    </div>
+    <div class="hidden lg:flex flex-col ml-10 text-2xl text-gray-400">
+        <h2>Name</h2>
         <div
-            class="flex text-sm gap-4 content-around pt-1 text-black text-opacity-40"
+            class="flex text-sm gap-2 content-around pt-1 text-black text-opacity-40"
         >
             <div class="flex justify-items-start">
                 <div class="flex">
@@ -24,9 +28,5 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div />
-    <div class="hidden lg:flex ml-10 text-2xl text-gray-400">
-        <h2>Name</h2>
     </div>
 </div>

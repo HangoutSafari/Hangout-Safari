41a26eefc358b54d6bd881fe631e9671e1a5eb1c
t0nkisa <antanitonev03@gmail.com>
2024-01-12 13:25:30 +0100

    auth call example

--- src/routes/example/+page.svelte
+++ src/routes/example/+page.svelte
@@ -4,14 +4,23 @@
   // data is the data from the page.server.js
   // export let data;
   // console.log(data);
-  onMount(getSession);
+  onMount(async () => {
+    console.log(await getSession());
+    console.log(await getAnimals());
+  });
   async function getSession() {
     const request = await fetch("http://localhost:3010/auth/session", {
       credentials: "include",
     });
     const response = await request.json();
-    if (response.status == 500) goto("/login");
-    return { message: `data from the server ${response}` };
+    return response;
+  }
+  async function getAnimals() {
+    const request = await fetch("http://localhost:3010/animals/auth", {
+      credentials: "include",
+    });
+    const response = await request.json();
+    return response;
   }
 </script>
 

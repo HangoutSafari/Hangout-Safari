3d034d5af9bca5c7363f7c675b89c0ac248fcc1f
t0nkisa <antanitonev03@gmail.com>
2024-01-17 14:25:08 +0100

    events load

--- src/lib/components/eventPage/DateCreated.svelte
+++ src/lib/components/eventPage/DateCreated.svelte
@@ -1,3 +1,6 @@
+<script>
+    export let created_at;
+</script>
 <div class="bg-transparent text-lg font-bold md:text-xl ml-2 text-gray-500">
-    Event created: dd/mm/yyyy
+    Event created: {created_at}
 </div>

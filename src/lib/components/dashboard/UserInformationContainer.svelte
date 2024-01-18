6625803df9043279cdbccd473cc066fb0c89e28c
Simon-Manasse <simo0047@hz.nl>
2024-01-18 08:27:26 +0100

    Fixed the oopsie woopsies

--- src/lib/components/dashboard/UserInformationContainer.svelte
+++ src/lib/components/dashboard/UserInformationContainer.svelte
@@ -64,10 +64,12 @@
             <div class="flex justify-center pt-4">
                 <h1 class="font-bold text-base mt-2 ml-2 pr-6">Latest Animal</h1>
                 <div class="h-14 w-14 mr-2 scale-125">
+                    {#if currentUser.count != 0}
                     <img
-                        src={AnimalsModels[animalsByDate[0].model_id].imagePath}
-                        alt="Profile Image"
-                        class=" bg-orange-400 rounded-lg p-1">
+                    src={AnimalsModels[animalsByDate[0].model_id].imagePath}
+                    alt="Profile Image"
+                    class=" bg-orange-400 rounded-lg p-1">
+                    {/if}
                 </div>
             </div>
             <div class="lg:hidden flex justify-center w-full items-center  self-center  my-2">
